067908e6233f416ff9578e6e5cb405e8d060cc87
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-10-29 16:24:47 +0100

    logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Logo(){
-    return(
+export default function Logo() {
+    return (
         <>
-        <Link
-        href="/"
-        >
-        <h1>LOGO</h1>
-        </Link>
+            <Link
+                href="/"
+            >
+                <Image
+                    src="/Images/logo.png"
+                    alt="Vercel logomark"
+                    width={100}
+                    height={100}
+                />
+            </Link>
         </>
     )
 }
